27f4e23c2125205462150919803fffad7b9a39aa
methawicaa_ <75575448+niepowiemcinazwy@users.noreply.github.com>
2025-02-25 12:22:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,44 +6,13 @@ import { SingleProject } from './Pages/SingleProject';
 import NoPage from './Pages/NoPage';
 import { Layout } from './Layout';
 
-import LoadingRocket from './images/loading-rocket.gif';
-
-import { useState } from 'react';
-
 import './index.css';
 import './resp.css';
 
 function App() {
 
-  const [loading, setLoading] = useState(true);
-  const [fadeOut, setFadeOut] = useState(false);
-
-  window.onload = () => {
-    
-    setLoading(true);
-
-    setTimeout(() => {
-
-      setFadeOut(true);
-
-      setTimeout(() => {
-
-        setLoading(false);
-
-      }, 300);
-
-    }, 1000);
-
-  };
-
   return (
     <BrowserRouter>
-      {loading ? (
-        <div className={`loading ${fadeOut ? 'fade-out' : ''}`}>
-          <img src={LoadingRocket} alt="Loading Rocket"></img>
-          <h1>Loading...</h1>
-        </div>
-      ) : (
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" index element={<Home />} />
@@ -54,7 +23,6 @@ function App() {
             <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
-      )}
     </BrowserRouter>
   );
 }
